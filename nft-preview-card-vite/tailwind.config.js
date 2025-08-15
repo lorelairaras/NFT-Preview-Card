fb3b2c51ed973cf97b83b0d27bbc54f15d6600a1
lorelairaras <lorelairaras@gmail.com>
2025-08-15 14:48:01 +0800

    Currently understanding tailwind

--- nft-preview-card-vite/tailwind.config.js
+++ nft-preview-card-vite/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          500: "hsl(215, 51%, 70%)",
+          800: "hsl(215, 32%, 27%)",
+          900: "hsl(216, 50%, 16%)",
+          950: "hsl(217, 54%, 11%)",
+        },
+        cyan: {
+          400: "hsl(178, 100%, 50%)",
+        },
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
